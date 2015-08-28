b4f4ef1cb71dfd0805b9ea1e4393251615c06950
Edwin Dover <edsdover@gmail.com>
2015-08-27 17:48:57 -0700

    assignment work

--- source/config/constants.js
+++ source/config/constants.js
@@ -2,7 +2,7 @@
 
 angular.module('chRepo')
 .constant('urls',{
-  'apiUrl': 'http://localhost:8000',
-  // 'https://fast-coast-4681.herokuapp.com'
+  // 'apiUrl': 'http://localhost:8000',
+  'apiUrl': 'ch-repo.herokuapp.com',
   'firebaseUrl': 'https://facebooknode.firebaseio.com/'
 });
