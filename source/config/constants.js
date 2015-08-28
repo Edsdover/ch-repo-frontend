bc21bfb5d766482e68516c30f5ebdc12f13ae96d
Edwin Dover <edsdover@gmail.com>
2015-08-27 18:27:15 -0700

    added bower to dependencies

--- source/config/constants.js
+++ source/config/constants.js
@@ -3,6 +3,6 @@
 angular.module('chRepo')
 .constant('urls',{
   // 'apiUrl': 'http://localhost:8000',
-  'apiUrl': 'ch-repo.herokuapp.com',
+  'apiUrl': 'https://ch-repo-backend.herokuapp.com',
   'firebaseUrl': 'https://facebooknode.firebaseio.com/'
 });
