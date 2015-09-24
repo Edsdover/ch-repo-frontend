298e236f11ed9f999640aa13df7a3e8d6845b9ff
Edwin Dover <edsdover@gmail.com>
2015-09-23 21:47:28 -0700

    mikes branch

--- source/views/projects/new.js
+++ source/views/projects/new.js
@@ -2,10 +2,17 @@
 
 angular.module('chRepo')
 .controller('NewProjectCtrl', function($scope, Project, sweet, $window){
+  $(document).ready(function() { // jshint ignore:line
+    $('#sel').change(function() { // jshint ignore:line
+        var currentVal = $('#projectTech').val(); // jshint ignore:line
+        $('#projectTech').val(currentVal + $(this).val() + ",   "); // jshint ignore:line
+    });
 
   $scope.newItem = true;
 
   $scope.submit = function(obj){
+    obj.notes = obj.notes;
+    obj.tech = $('#projectTech').val(); // jshint ignore:line
     Project.create(obj)
     .success(function(data){
       sweet.show('Project Save Success', 'Success, Your project is saved!', 'success');
@@ -15,4 +22,5 @@ angular.module('chRepo')
       $window.swal({title: 'Project Save Error', text: 'Warning, there was a problem saving your project.', type: 'error'});
     });
   };
+});
 });
